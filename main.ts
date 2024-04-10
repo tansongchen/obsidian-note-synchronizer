3d76b9975379bdde4267adc9450bc8ac620eb29f
Belen Buccolo <35378809+belenbuccolo@users.noreply.github.com>
2024-04-10 10:58:03 -0300

    fix: cross platform media sync and only sync media of anki notes (#44)
    * style: run npm format and npm install
    
    * fix: cross platform media sync and only sync media of anki notes
    
    * Add latest changes

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { normalizePath, Notice, Plugin } from 'obsidian';
+import { normalizePath, Notice, Plugin, TFile, TFolder, Vault } from 'obsidian';
 import Anki, { AnkiError } from 'src/anki';
 import Note, { NoteManager } from 'src/note';
 import { MediaManager } from 'src/media';
@@ -6,6 +6,7 @@ import locale from 'src/lang';
 import { NoteDigest, NoteState, NoteTypeDigest, NoteTypeState } from 'src/state';
 import AnkiSynchronizerSettingTab, { Settings, DEFAULT_SETTINGS } from 'src/setting';
 import { version } from './package.json';
+import { MD5 } from 'object-hash';
 
 interface Data {
   version: string;
@@ -21,6 +22,7 @@ export default class AnkiSynchronizer extends Plugin {
   noteManager = new NoteManager(this.settings);
   noteState = new NoteState(this);
   noteTypeState = new NoteTypeState(this);
+  
 
   async onload() {
     // Recover data from local file
@@ -42,17 +44,17 @@ export default class AnkiSynchronizer extends Plugin {
   configureUI() {
     // Add import note types command
     this.addCommand({
-      id: "import",
+      id: 'import',
       name: locale.importCommandName,
-      callback: async () => await this.importNoteTypes(),
+      callback: async () => await this.importNoteTypes()
     });
     this.addRibbonIcon('enter', locale.importCommandName, async () => await this.importNoteTypes());
 
     // Add synchronize command
     this.addCommand({
-      id: "synchronize",
+      id: 'synchronize',
       name: locale.synchronizeCommandName,
-      callback: async () => await this.synchronize(),
+      callback: async () => await this.synchronize()
     });
     this.addRibbonIcon(
       'sheets-in-box',
@@ -70,7 +72,7 @@ export default class AnkiSynchronizer extends Plugin {
       version: version,
       settings: this.settings,
       noteState: Object.fromEntries(this.noteState),
-      noteTypeState: Object.fromEntries(this.noteTypeState),
+      noteTypeState: Object.fromEntries(this.noteTypeState)
     });
   }
 
@@ -81,7 +83,7 @@ export default class AnkiSynchronizer extends Plugin {
 
   // Retrieve template information from Obsidian core plugin "Templates"
   getTemplatePath() {
-    const templatesPlugin = (this.app as any).internalPlugins?.plugins["templates"];
+    const templatesPlugin = (this.app as any).internalPlugins?.plugins['templates'];
     if (!templatesPlugin?.enabled) {
       new Notice(locale.templatesNotEnabledNotice);
       return;
@@ -131,16 +133,43 @@ export default class AnkiSynchronizer extends Plugin {
     const templatesPath = this.getTemplatePath();
     if (templatesPath === undefined) return;
     new Notice(locale.synchronizeStartNotice);
-    const allFiles = this.app.vault.getMarkdownFiles();
     const state = new Map<number, [NoteDigest, Note]>();
-    for (const file of allFiles) {
-      // ignore templates
-      if (file.path.startsWith(templatesPath)) continue;
-      // read and validate content
-      const content = await this.app.vault.cachedRead(file);
+
+    // getActiveViewOfType
+    const activeFile = this.app.workspace.getActiveFile();
+    const folderPath = activeFile?.parent?.path
+    const deck = folderPath?.replace(/\//g, '::') || 'Obsidian';
+
+    const folder = this.app.vault.getAbstractFileByPath(folderPath || "/") as any
+    const files = folder?.children as any
+
+    console.log(`Found ${files.length} files in obsidian folder`, folder);
+
+    const notesInfoResponse = await this.anki.notesInfoByDeck(deck)
+
+    console.log("Found notes in Anki", notesInfoResponse);
+
+    
+    for (const file of files) {
       const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter;
+
       if (!frontmatter) continue;
+
+      const content = await this.app.vault.cachedRead(file);
       const media = this.app.metadataCache.getFileCache(file)?.embeds;
+      
+      const [obsidianNote, mediaNameMap] = this.noteManager.validateNote(
+        file,
+        frontmatter,
+        content,
+        media,
+        this.noteTypeState
+      );
+
+      if (!obsidianNote) continue;
+
+      console.log(`Validated note ${obsidianNote.title()}`, obsidianNote);
+
       if (media) {
         for (const item of media) {
           this.noteState.handleAddMedia(
@@ -148,26 +177,40 @@ export default class AnkiSynchronizer extends Plugin {
           );
         }
       }
-      const [note, mediaNameMap] = this.noteManager.validateNote(
-        file,
-        frontmatter,
-        content,
-        media,
-        this.noteTypeState
-      );
-      if (!note) continue;
-      if (note.nid === 0) {
+
+      const correspondingAnkiNote = notesInfoResponse.find((note: any) => note.noteId === frontmatter.nid);
+
+      // Merge anki tags and obsidian tags
+      const obsidianTags = frontmatter.tags || []
+      const ankiTags = correspondingAnkiNote?.tags || [];
+      const mergedTags = [...new Set([...obsidianTags, ...ankiTags])];
+
+      const tagsBeforeHash = MD5(frontmatter.tags);
+      const tagsAfterHash = MD5(mergedTags);
+      const shouldUpdateTags = tagsBeforeHash !== tagsAfterHash;
+
+      
+    
+      if (obsidianNote.nid === 0) {
         // new file
-        const nid = await this.noteState.handleAddNote(note);
+        const nid = await this.noteState.handleAddNote(obsidianNote);
         if (nid === undefined) {
           new Notice(locale.synchronizeAddNoteFailureNotice(file.basename));
           continue;
         }
-        note.nid = nid;
-        this.app.vault.modify(file, this.noteManager.dump(note, mediaNameMap));
+        obsidianNote.nid = nid;
+        this.app.vault.modify(file, this.noteManager.dump(obsidianNote, mediaNameMap));
       }
-      state.set(note.nid, [note.digest(), note]);
+
+      if (shouldUpdateTags) {
+        obsidianNote.tags = mergedTags;
+        this.app.vault.modify(file, this.noteManager.dump(obsidianNote, mediaNameMap));
+      }
+      
+
+      state.set(obsidianNote.nid, [obsidianNote.digest(), obsidianNote]);
     }
+
     await this.noteState.change(state);
     await this.save();
     new Notice(locale.synchronizeSuccessNotice);
--- src/state.ts
+++ src/state.ts
@@ -16,22 +16,24 @@ abstract class State<K, V, I = undefined> extends Map<K, V> {
     this.anki = plugin.anki;
   }
 
-  async change(state: Map<K, V | [V, I]>) {
-    const _keys = [...this.keys()];
-    const keys = [...state.keys()];
-    for (const [key, wrap] of state.entries()) {
-      if (Array.isArray(wrap)) {
-        const [value, info] = wrap;
-        await this.update(key, value, info);
-        this.set(key, value);
+  async change(newState: Map<K, V | [V, I]>) {
+    const existingKeys = [...this.keys()];
+    const newKeys = [...newState.keys()];
+
+    // Iterate over the new state
+    for (const [key, value] of newState.entries()) {
+      if (Array.isArray(value)) {
+        const [digest, info] = value;
+        await this.update(key, digest, info);
+        this.set(key, digest);
       } else {
-        await this.update(key, wrap);
-        this.set(key, wrap);
+        await this.update(key, value);
+        this.set(key, value);
       }
     }
-    for (const key of _keys.filter(x => !keys.includes(x))) {
-      this.delete(key);
-    }
+    // for (const key of existingKeys.filter(x => !newKeys.includes(x))) {
+    //   this.delete(key);
+    // }
   }
 
   abstract update(key: K, value: V, info?: I): Promise<void>;
@@ -58,7 +60,7 @@ export class NoteTypeState extends State<number, NoteTypeDigest> {
     return super.delete(key);
   }
 
-  async update(key: number, value: NoteTypeDigest) {
+  async update(key: number, digest: NoteTypeDigest) {
     if (this.has(key)) {
       this.delete(key);
     }
@@ -69,15 +71,15 @@ export class NoteTypeState extends State<number, NoteTypeDigest> {
       date: '{{date}} {{time}}'
     } as FrontMatter;
     const pseudoFields: Record<string, string> = {};
-    value.fieldNames.map(x => (pseudoFields[x] = '\n\n'));
+    digest.fieldNames.map(x => (pseudoFields[x] = '\n\n'));
     const templateNote = new Note(
-      value.name,
+      digest.name,
       this.templateFolderPath!,
-      value.name,
+      digest.name,
       pseudoFrontMatter,
       pseudoFields
     );
-    const templatePath = `${this.templateFolderPath}/${value.name}.md`;
+    const templatePath = `${this.templateFolderPath}/${digest.name}.md`;
     const maybeTemplate = this.plugin.app.vault.getAbstractFileByPath(templatePath);
     if (maybeTemplate !== null) {
       await this.plugin.app.vault.modify(
@@ -102,29 +104,34 @@ export class NoteState extends State<number, NoteDigest, Note> {
   }
 
   // Existing notes may have 3 things to update: deck, fields, tags
-  async update(key: number, value: NoteDigest, info: Note) {
+  async update(key: number, digest: NoteDigest, info: Note) {    
+
     const current = this.get(key);
     if (!current) return;
-    if (current.deck !== value.deck) {
+    if (current.deck !== digest.deck) {
       // updating deck
-      this.updateDeck(key, current, value, info);
+      this.updateDeck(info);
     }
-    if (current.hash !== value.hash) {
+    if (current.hash !== digest.hash) {
       // updating fields
-      this.updateFields(key, current, value, info);
+      this.updateFields(info);
     }
     // Check for null case
     if (current.tags === null) current.tags = [];
-    if (value.tags === null) value.tags = [];
-    if (current.tags.length !== value.tags.length || current.tags.some((item, index) => item !== value.tags[index])) {
+    if (digest.tags === null) digest.tags = [];
+    if (
+      current.tags.length !== digest.tags.length ||
+      current.tags.some((item, index) => item !== digest.tags[index])
+    ) {
       // updating tags
-      this.updateTags(key, current, value, info);
+      this.updateTags(info);
     }
   }
 
-  async updateDeck(key: number, current: NoteDigest, value: NoteDigest, note: Note) {
+  async updateDeck(note: Note) {
     const deck = note.renderDeckName();
     const notesInfoResponse = await this.anki.notesInfo([note.nid]);
+
     if (!Array.isArray(notesInfoResponse)) {
       return;
     }
@@ -146,7 +153,7 @@ export class NoteState extends State<number, NoteDigest, Note> {
     new Notice(locale.synchronizeChangeDeckFailureNotice(note.title()));
   }
 
-  async updateFields(key: number, current: NoteDigest, value: NoteDigest, note: Note) {
+  async updateFields(note: Note) {
     const fields = this.formatter.format(note);
     console.log(`Updating fields for ${note.title()}`, fields);
     const updateFieldsResponse = await this.anki.updateFields(note.nid, fields);
@@ -154,12 +161,11 @@ export class NoteState extends State<number, NoteDigest, Note> {
     new Notice(locale.synchronizeUpdateFieldsFailureNotice(note.title()));
   }
 
-  async updateTags(key: number, current: NoteDigest, nextValue: NoteDigest, note: Note) {
+  async updateTags(note: Note) {
     let updateTagsResponse = null;
     console.log(`Updating tags for ${note.title()}`, note.tags);
     updateTagsResponse = await this.anki.updateNoteTags(note.nid, note.tags);
-    if (updateTagsResponse)
-      new Notice(locale.synchronizeUpdateTagsFailureNotice(note.title()));
+    if (updateTagsResponse) new Notice(locale.synchronizeUpdateTagsFailureNotice(note.title()));
   }
 
   delete(key: number) {
